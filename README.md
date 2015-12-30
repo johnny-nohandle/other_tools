4a48aceb97400970e32a3478830c55e3e19104d0
JP <random.padding@gmail.com>
2015-12-30 00:44:25 -0500

    Initial commit
    sha256sum and the README

---
+++ README.md
@@ -0,0 +1,6 @@
+Other_tools
+===========
+
+Description
+-----------
+Just some scripts that have helped me in the past that I thought I'd share. Each should have its own usage.
---
+++ sha256sum.py
@@ -0,0 +1,22 @@
+import hashlib
+from sys import argv
+
+
+def sha256filehash(filename):
+    with open(filename, 'rb') as filehandle:
+        filebuf = filehandle.read()
+        hashret = hashlib.sha256(filebuf).hexdigest()
+        print "SHA256:%s : %s" % (hashret, filename)
+        filehandle.close()
+
+
+def main():
+    if len(argv) != 2:
+        print "This script takes 1 argument. Usage: sha256sum.py path\\to\\file"
+        exit(0)
+    (scriptname, filename) = argv
+    sha256filehash(filename)
+    return 0
+
+if __name__ == '__main__':
+        main()
